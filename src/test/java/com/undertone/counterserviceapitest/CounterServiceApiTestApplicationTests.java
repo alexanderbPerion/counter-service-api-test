450ba06441f22fd42d0737c3c60fd48bcbf87f3f
alexanderbPerion <alexanderb@perion.com>
2022-12-25 13:43:30 +0200

    .

--- src/test/java/com/undertone/counterserviceapitest/CounterServiceApiTestApplicationTests.java
+++ src/test/java/com/undertone/counterserviceapitest/CounterServiceApiTestApplicationTests.java
@@ -1,13 +1,16 @@
 package com.undertone.counterserviceapitest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class CounterServiceApiTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("AAAAAAAA");
 	}
 
 }
